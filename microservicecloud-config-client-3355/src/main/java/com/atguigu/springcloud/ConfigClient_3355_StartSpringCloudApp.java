63b5677e5d46172be824c0cc2b7d64df7f53860d
yudongjie <iyudongjie@163.com>
2019-11-11 22:42:36 +0800

    commit config

---
+++ microservicecloud-config-client-3355/src/main/java/com/atguigu/springcloud/ConfigClient_3355_StartSpringCloudApp.java
@@ -0,0 +1,17 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: yudongjie
+ * @CreateDate: 2019-11-11 22:05
+ */
+@SpringBootApplication
+
+public class ConfigClient_3355_StartSpringCloudApp {
+    public static void main(String[] args){
+        SpringApplication.run(ConfigClient_3355_StartSpringCloudApp.class, args);
+
+    }
+}
